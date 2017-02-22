1505dd6e5778838b42b2c5814d5dd4104358234f
Geo Perez <geovanni.perez@gmail.com>
2017-02-22 13:59:33 -0600

    Support Stale computers

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.DirectoryServices;
 using System.DirectoryServices.ActiveDirectory;
 using System.Linq;
@@ -10,10 +11,8 @@ namespace LdapPack
 {
     class Program
     {
-        public static Dictionary<string, DateTime> UsersLastLogOnDate()
+        private static void GetLdapInfo(Action<SearchResult> callback, string filter, string[] propertiesToLoad)
         {
-            var lastLogins = new Dictionary<string, DateTime>();
-
             foreach (DomainController controller in Domain.GetCurrentDomain().DomainControllers)
             {
                 $"Querying: {controller.Name}".Info();
@@ -25,45 +24,59 @@ namespace LdapPack
                         using (var searcher = new DirectorySearcher(directoryEntry))
                         {
                             searcher.PageSize = 1000;
-                            searcher.Filter =
-                                "(&(objectCategory=person)(objectClass=user)(!(userAccountControl:1.2.840.113556.1.4.803:=2)))";
-                            searcher.PropertiesToLoad.AddRange(new[] {"distinguishedName", "lastLogon"});
+                            searcher.Filter = filter;
+                            searcher.PropertiesToLoad.AddRange(propertiesToLoad);
 
                             foreach (SearchResult searchResult in searcher.FindAll())
                             {
-                                if (!searchResult.Properties.Contains("lastLogon")) continue;
-
-                                var lastLogOn = DateTime.FromFileTime((long) searchResult.Properties["lastLogon"][0]);
-                                var username = searchResult.Properties["distinguishedName"][0].ToString();
-
-                                if (lastLogins.ContainsKey(username))
-                                {
-                                    if (lastLogOn > lastLogins[username])
-                                    {
-                                        lastLogins[username] = lastLogOn;
-                                    }
-                                }
-                                else
-                                {
-                                    lastLogins.Add(username, lastLogOn);
-                                }
+                                callback(searchResult);
                             }
                         }
 
                     }
                 }
-                catch
+                catch (Exception ex)
                 {
-                    // Domain controller is down or not responding
-                    $"Domain controller {controller.Name} is not responding.".Debug();
+                    ex.Message.Error();
+                    $"Domain controller {controller.Name} is not responding.".Error();
                 }
             }
-            return lastLogins;
         }
 
-        static void Main(string[] args)
+        private static void GetStaleUsers()
         {
-            var data = UsersLastLogOnDate();
+            var data = new Dictionary<string, DateTime>();
+
+            var callback = new Action<SearchResult>((searchResult) =>
+            {
+                if (!searchResult.Properties.Contains("lastLogon")) return;
+
+                var lastLogOn = DateTime.FromFileTime((long) searchResult.Properties["lastLogon"][0]);
+                var username = searchResult.Properties["distinguishedName"][0].ToString();
+
+                if (data.ContainsKey(username))
+                {
+                    if (lastLogOn > data[username])
+                    {
+                        data[username] = lastLogOn;
+                    }
+                }
+                else
+                {
+                    data.Add(username, lastLogOn);
+                }
+            });
+
+            GetLdapInfo(callback,
+                "(&(objectCategory=person)(objectClass=user)(!(userAccountControl:1.2.840.113556.1.4.803:=2)))",
+                new[] {"distinguishedName", "lastLogon" });
+
+            if (data.Count == 0)
+            {
+                "Found no data".Warn();
+                return;
+            }
+
             $"Found {data.Count} records".Info();
 
             var days = "How many days before?".ReadNumber(30);
@@ -74,18 +87,121 @@ namespace LdapPack
                 $"{item.Key} - {item.Value}".Info();
 
             $"Filtered {filtered.Count()} records".Info();
+            
+            if ("Do you want to export to CSV? (y)".ReadKey().Key != ConsoleKey.Y) return;
 
-            var key = "Do you want to export to CSV (y)".ReadKey();
+            var filename = Runtime.GetDesktopFilePath("ldap-stale-users.csv");
+            CsvWriter.SaveRecords(filtered, filename);
+            $"Export done {filename}".Info();
+            
+            if ("Do you want to open the file? (y)".ReadKey().Key != ConsoleKey.Y) return;
 
-            if (key.Key == ConsoleKey.Y)
+            (new Process()
             {
-                var filename = Runtime.GetDesktopFilePath("ldap.csv");
-                CsvWriter.SaveRecords(filtered, filename);
-                $"Exporting done {filename}".Info();
+                StartInfo = new ProcessStartInfo {FileName = filename}
+            }).Start();
+        }
+
+        private static void GetStaleComputers()
+        {
+            var data = new Dictionary<string, Tuple<DateTime, string>>();
+
+            var callback = new Action<SearchResult>((searchResult) =>
+            {
+                if (!searchResult.Properties.Contains("PwdLastSet")) return;
+
+                var pwdLastSet = DateTime.FromFileTime((long) searchResult.Properties["PwdLastSet"][0]);
+                var username = searchResult.Properties["distinguishedName"][0].ToString();
+                var operatingsystem = searchResult.Properties.GetValue("operatingsystem");
+                
+                if (data.ContainsKey(username))
+                {
+                    if (pwdLastSet > data[username].Item1)
+                    {
+                        data[username] = new Tuple<DateTime, string>(pwdLastSet, operatingsystem);
+                    }
+                }
+                else
+                {
+                    data.Add(username, new Tuple<DateTime, string>(pwdLastSet, operatingsystem));
+                }
+            });
+
+            GetLdapInfo(callback,
+                "(&(objectCategory=computer)(!(userAccountControl:1.2.840.113556.1.4.803:=2))(!(isCriticalSystemObject=TRUE)))",
+                new[] {"PwdLastSet", "distinguishedName", "operatingsystem", "lastLogonTimeStamp " });
+
+            if (data.Count == 0)
+            {
+                "Found no data".Warn();
+                return;
             }
 
-            "Press any key to exit...".Info();
-            Terminal.ReadLine();
+            $"Found {data.Count} records".Info();
+            
+            var days = "How many days before?".ReadNumber(30);
+            var filtered = data.Select(x => new
+            {
+                Computer = x.Key,
+                OperatingSystem = x.Value.Item2,
+                Stale = x.Value.Item1 < DateTime.Now.AddDays(-days) ? "TRUE" : "FALSE"
+            }).ToList();
+
+            foreach (var item in filtered)
+                $"{item.Computer} - {item.OperatingSystem} - {item.Stale}".Info();
+
+            if ("Do you want to export to CSV? (y)".ReadKey().Key != ConsoleKey.Y) return;
+
+            var filename = Runtime.GetDesktopFilePath("ldap-stale-computers.csv");
+            CsvWriter.SaveRecords(filtered, filename);
+
+            $"Export done {filename}".Info();
+
+            if ("Do you want to open the file? (y)".ReadKey().Key != ConsoleKey.Y) return;
+
+            (new Process()
+            {
+                StartInfo = new ProcessStartInfo { FileName = filename }
+            }).Start();
+        }
+
+        static void Main(string[] args)
+        {
+            Runtime.WriteWelcomeBanner();
+
+            var options = new Dictionary<ConsoleKey, string>()
+            {
+                {ConsoleKey.U, "Get stale users"},
+                {ConsoleKey.C, "Get stale computers"}
+            };
+
+            var continueLoop = true;
+
+            while (continueLoop)
+            {
+                var response = "Choose an option".ReadPrompt(options, "Press any key to close...");
+
+                switch (response.Key)
+                {
+                    case ConsoleKey.U:
+                        GetStaleUsers();
+                        break;
+                    case ConsoleKey.C:
+                        GetStaleComputers();
+                        break;
+                    default:
+                        continueLoop = false;
+                        break;
+                }
+            }
+        }
+    }
+
+    static class Extensions
+    {
+        public static string GetValue(this ResultPropertyCollection coll, string key)
+        {
+            return coll[key].Count > 0 ? coll[key][0].ToString() : null;
         }
     }
 }
