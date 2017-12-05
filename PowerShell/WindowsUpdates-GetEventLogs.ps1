3c7c0cdea39a82d162a6894c6c721c2fe2bb24ed
Israel <arnulfo_israel@hotmail.com>
2017-12-05 13:45:26 -0600

    PowerShell folder and windows updates scripts

---
+++ PowerShell/WindowsUpdates-GetEventLogs.ps1
@@ -0,0 +1,15 @@
+﻿$date = Get-Date
+$date = $date.AddDays(-5)
+$entries = $(
+    "GDLA-LT-170714A.ad.unosquare.com"
+    # ""
+)
+
+$Search = Get-EventLog -LogName System -Source "Microsoft-Windows-WindowsUpdateClient" # -ComputerName $entries # To make it remote
+$Search | where {$_.InstanceId -eq 19} | 
+        Select-Object MachineName,
+        @{ name = "Date"; expression = {$_.TimeGenerated} },
+        @{ name = "Operation"; expression = { $_.Message.split(":")[0]}},
+        @{ name = "Title"; expression = { $_.Message.split(":")[2]}} | 
+        Where-Object { $_.Date -gt $date } |
+        Export-Csv -NoType "$Env:userprofile\Desktop\WindowsUpdates.csv"
---
+++ PowerShell/WindowsUpdates-MicrosoftUpdateSession.ps1
@@ -0,0 +1,45 @@
+﻿$entries = @(
+    "gdl-dc-01.ad.unosquare.com"
+    # "GDLA-LT-170714A.ad.unosquare.com",
+    # "gdl-vhost.ad.unosquare.com"
+)
+foreach($entry in $entries)
+{
+    $result = Invoke-Command -ComputerName $entry -ScriptBlock {
+        $Session = New-Object -ComObject "Microsoft.Update.Session"
+        $Searcher = $Session.CreateUpdateSearcher()
+        $historyCount = $Searcher.GetTotalHistoryCount()
+        
+        $search = $Searcher.QueryHistory(0, $historyCount)
+        $search
+    }
+
+    $date = Get-Date
+    $date = $date.AddDays(-10)
+
+    $result | Select-Object Date,
+        @{
+            name = "Operation"; 
+            expression = {
+                switch ($_.operation) {
+                    1 {"Installation"};
+                    2 {"Uninstallation"};
+                    3 {"Other"}
+                }
+            }
+        },   
+        @{
+            name = "Status"; 
+            expression = {
+                switch ($_.resultcode) {       
+                    1 {"In Progress"}; 
+                    2 {"Succeeded"}; 
+                    3 {"Succeeded With Errors"};       
+                    4 {"Failed"}; 
+                    5 {"Aborted"}
+                }
+            }
+        } |
+        Where-Object { $_.Date -gt $date } |
+        Export-Csv -NoType "$Env:userprofile\Desktop\WindowsUpdates_$entry.csv"
+}
