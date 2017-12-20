0183369770ae2b3c95a4d746d6b019cf1116b88b
Israel <arnulfo_israel@hotmail.com>
2017-12-20 16:56:42 -0600

    Powershell updates modified and added backup

---
+++ PowerShell/WindowsBackups.ps1
@@ -0,0 +1,46 @@
+﻿# Array of servers.
+$entries = @( 
+    "jac-vhost.ad.unosquare.com",
+    "leon-vhost.ad.unosquare.com",
+    "allius-vhost.ad.unosquare.com",
+    "gdl-vhost.ad.unosquare.com",
+    "gdl-devlab.ad.unosquare.com"
+)
+
+# Script Block that its going to be executed in each server.
+$sc = { 
+    # Adds windows.serverbackup snap-in to the current session.
+    add-pssnapin windows.serverbackup 
+    # Gets the history of the backup operations.
+    $Summary = Get-WBSummary 
+    $Summary
+}
+
+# Gets the hostname
+$hostname = hostname 
+
+foreach($entry in $entries)
+{
+    # Checks if the entry is localhost
+    $remote = $entry -notmatch $hostname 
+    
+    # Executes the Script Block in each server
+    if($remote) {
+        # If remote computer
+        $result = Invoke-Command -ComputerName $entry -ScriptBlock $sc
+    } else { 
+        # Localhost
+        $result = Invoke-Command -ScriptBlock $sc
+    }
+
+    $date = Get-Date
+    $date = $date.AddDays(-10)
+
+    # Selects the important information
+    $output = $result | Select-Object PSComputerName, 
+        NextBackupTime, LastSuccessfulBackupTime, LastBackupTime | 
+        Where-Object { $_.LastBackupTime -gt $date } 
+
+    # Sends the output to a file
+    $output | Out-File -NoType "$Env:userprofile\Desktop\Backups Logs\BackupLog_$entry.txt" -width 300
+}
--- PowerShell/WindowsUpdates-MicrosoftUpdateSession.ps1
+++ PowerShell/WindowsUpdates-MicrosoftUpdateSession.ps1
@@ -1,4 +1,5 @@
-﻿$entries = @(
+﻿# Array of servers
+$entries = @(
     "gdl-dc-01",
     "jac-dc-01",
     "allius-dc-01",
@@ -6,35 +7,73 @@
     "unocorp-dc-01",
     "unocorp-dc-02"
 )
+# Script Block that its going to be executed in each server
 $sc = { 
-    $Session = New-Object -ComObject "Microsoft.Update.Session"
+    # Creates a Microsoft Update Session
+    $Session = New-Object -ComObject "Microsoft.Update.Session" 
     $Searcher = $Session.CreateUpdateSearcher()
     $historyCount = $Searcher.GetTotalHistoryCount()
-
+    # Gets the information of the installed updates
     $search = $Searcher.QueryHistory(0, $historyCount)
-    $search 
-}
-$hostname = hostname
+    # Gets the information of the available updates
+    $searchresult = $Searcher.Search("IsInstalled=0")
 
-foreach($entry in $entries)
-{
-    $remote = $entry -notmatch $hostname
+    # Initialize an array
+    $updates = @()
 
-    if($remote) {
-        #If remote computer
-        $result = Invoke-Command -ComputerName $entry -ScriptBlock $sc
-    } else { 
-        #Localhost
-        Get-Date
-        $result = Invoke-Command -ScriptBlock $sc
+    foreach($Update in $searchresult.Updates){
+        $updates += [pscustomobject]@{
+            Title = $Update.Title
+            Date = $Update.LastDeploymentChangeTime
+            IsDownloaded = $Update.IsDownloaded
+            IsInstalled = $Update.IsInstalled 
+            Url = $($Update.MoreInfoUrls)
+        } 
     }
 
+    $search
+    $updates
+}
+
+# Gets the hostname
+$hostname = hostname 
+# Iterates through the array of servers
+foreach($entry in $entries)
+{
+    # Checks if the entry is a ipaddress
+    $ipaddress = [bool]($entry -as [ipaddress])
+    # Checks if the entry is localhost
+    $remote = $entry -notmatch $hostname 
+
+    # Executes the Script Block in each server
+    if($ipaddress){
+        # If Ipaddress
+        # (Get-Credential).Password | ConvertFrom-SecureString | Out-File "$Env:userprofile\Desktop\pass.txt"
+        $User = $entry+"\"+$env:username
+        $File = "$Env:userprofile\Desktop\pass.txt"
+        $MyCredential = New-Object -TypeName System.Management.Automation.PSCredential `
+         -ArgumentList $User, (Get-Content $File | ConvertTo-SecureString)
+        
+        # Adds the ipaddress to the trustedhosts
+        Set-Item WSMan:\localhost\Client\TrustedHosts –Value $entry -Concatenate -Force
+        Restart-Service WinRM
+        $result = Invoke-Command -ComputerName $entry -Credential (Get-Credential -Credential $MyCredential) -ScriptBlock $sc
+    }
+    elseif($remote) {
+        # If remote computer
+        $result = Invoke-Command -ComputerName $entry -ScriptBlock $sc
+    } else { 
+        # Localhost
+        $result = Invoke-Command -ScriptBlock $sc
+    }
+    
     $date = Get-Date
     $date = $date.AddDays(-10)
-
-    $output = $result | Select-Object Date,
+    $result
+    # Selects the important information and switches the number of operation and resultcode to text
+    $output = $result | Select-Object Title, Date,
         @{
-            name = "Operation"; 
+            name = "Operation";
             expression = {
                 switch ($_.operation) {
                     1 {"Installation"};
@@ -54,14 +93,24 @@ foreach($entry in $entries)
                     5 {"Aborted"}
                 }
             }
-        } ,title |
-        Where-Object { $_.Date -gt $date } 
+        } |
+        Where-Object { $_.Date -gt $date -and $_.Status -ne $null} | Format-List
 
+    $AvailableUpdates = $result | Select-Object Title, KB, Date, MaxDownloadSize, IsDownloaded, IsInstalled, Url | Where-Object { $_.IsInstalled -eq $false}  #Selects the updates that are not installed
+    
     if($output -eq $null)
     {
         $obj = @{ 'Message' = "There is no updates information. Please check.";}
         $output = New-Object -TypeName PSObject -Property $obj
     }
 
-    $output | Export-Csv -NoType "$Env:userprofile\Desktop\Windows Updates Logs\WindowsUpdates_$entry.csv"
+    if($AvailableUpdates -eq $null)
+    {
+        $obj = @{ 'Message' = "There is no available updates.";}
+        $AvailableUpdates = New-Object -TypeName PSObject -Property $obj
+    }
+    
+    # Sends the output to a file
+    $output | Out-File "$Env:userprofile\Desktop\Windows Updates Logs\WindowsUpdates_$entry.csv" -width 300
+    $AvailableUpdates | Out-File  "$Env:userprofile\Desktop\Windows Updates Logs\$entry.AvailableWindowsUpdates.csv" -width 120
 }
